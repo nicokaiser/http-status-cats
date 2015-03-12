77a2096c26c7c4d999696df35ff49ec16e3d46a8
Nico Kaiser <nico@kaiser.me>
2015-03-12 18:13:08 +0100

    Put our middleware before the request handler to ensure it gets called in "ok" cases.

--- examples/express-server.js
+++ examples/express-server.js
@@ -3,10 +3,10 @@ var statusCat = require('../');
 
 var app = express();
 
+app.use(statusCat());
+
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
-
-app.use(statusCat());
 
 app.listen(3000);
